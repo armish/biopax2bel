5d711fb3eb1dd1482db358790dde5ddffc389c50
B. Arman Aksoy <arman@aksoy.org>
2015-01-20 16:02:30 -0500

    fixed the logging settings; added commons-lang to the dependencies; started outputing a valid BEL file. TODO: Conversions themselves.

--- src/main/java/org/mskcc/cbio/bigmech/BioPAX2BELMain.java
+++ src/main/java/org/mskcc/cbio/bigmech/BioPAX2BELMain.java
@@ -6,10 +6,16 @@ import org.apache.commons.logging.LogFactory;
 import org.biopax.paxtools.io.SimpleIOHandler;
 import org.biopax.paxtools.model.Model;
 import org.mskcc.cbio.bigmech.converter.BioPAX2BELConverter;
+import org.mskcc.cbio.bigmech.util.BELHeaderWrapper;
 import org.openbel.bel.model.BELDocument;
+import org.openbel.framework.common.bel.converters.BELDocumentConverter;
+import org.openbel.framework.common.belscript.BELScriptExporter;
+import org.openbel.framework.common.model.Document;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class BioPAX2BELMain {
     private static Log log = LogFactory.getLog(BioPAX2BELMain.class);
@@ -19,8 +25,12 @@ public class BioPAX2BELMain {
         final CommandLineParser clParser = new GnuParser();
         Options gnuOptions = new Options();
         gnuOptions
-                .addOption("i", "input", true, "BioPAX file [required]")
-                .addOption("o", "output", true, "Output file (BEL file) [required]")
+            .addOption("i", "input", true, "BioPAX file [required]")
+            .addOption("o", "output", true, "Output file (BEL file) [required]")
+            .addOption("ha", "header-author", true, "BEL Header: Author name (default: null)")
+            .addOption("hd", "header-description", true, "BEL Header: Description (default: 'Converted from BioPAX on `date`)")
+            .addOption("hn", "header-name", true, "BEL Header: Name (default: 'Document #UUID'")
+            .addOption("hv", "header-version", true, "BEL Header: Version (default: 1.0)")
         ;
 
         try {
@@ -38,10 +48,33 @@ public class BioPAX2BELMain {
                 log.info("Converting the BioPAX model into BEL...");
                 // Convert the BioPAX file into a BEL model
                 BioPAX2BELConverter converter = new BioPAX2BELConverter();
+                BELHeaderWrapper belHeaderWrapper = converter.getBelHeaderWrapper();
+
+                // update header if we have information
+                if(commandLine.hasOption("ha")) {
+                    belHeaderWrapper.setAuthors(commandLine.getOptionValue("ha"));
+                }
+                if(commandLine.hasOption("hd")) {
+                    belHeaderWrapper.setDescription(commandLine.getOptionValue("hd"));
+                }
+                if(commandLine.hasOption("hn")) {
+                    belHeaderWrapper.setName(commandLine.getOptionValue("hn"));
+                }
+                if(commandLine.hasOption("hv")) {
+                    belHeaderWrapper.setVersion(commandLine.getOptionValue("hv"));
+                }
+                // Done setting the header values
+
+                // Now the do the coversion
                 BELDocument belDocument = converter.convert(bpModel);
 
                 log.info("Conversion done, now writing the BEL model into a file:" + outputFile);
-                // TODO: Serialize the model into the file
+                BELScriptExporter belScriptExporter = new BELScriptExporter();
+                FileOutputStream os = new FileOutputStream(outputFile);
+                BELDocumentConverter belDocumentConverter = new BELDocumentConverter();
+                Document convertedDoc = belDocumentConverter.convert(belDocument);
+                belScriptExporter.export(convertedDoc, os);
+                os.close();
                 System.exit(0);
             } else {
                 HelpFormatter helpFormatter = new HelpFormatter();
@@ -55,6 +88,9 @@ public class BioPAX2BELMain {
             helpFormatter.printHelp(helpText, gnuOptions);
             System.exit(-1);
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
         }
--- src/main/java/org/mskcc/cbio/bigmech/converter/BioPAX2BELConverter.java
+++ src/main/java/org/mskcc/cbio/bigmech/converter/BioPAX2BELConverter.java
@@ -5,13 +5,22 @@ import org.biopax.paxtools.model.level3.BiochemicalReaction;
 import org.mskcc.cbio.bigmech.util.BELHeaderWrapper;
 import org.mskcc.cbio.bigmech.util.BELUtils;
 import org.openbel.bel.model.BELDocument;
+import org.openbel.bel.model.BELStatement;
+import org.openbel.bel.model.BELStatementGroup;
+import org.openbel.framework.core.compiler.SymbolWarning;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 
 public class BioPAX2BELConverter {
     private BELUtils belUtils = new BELUtils();
-    private BELHeaderWrapper belHeaderWrapper = new BELHeaderWrapper(UUID.randomUUID().toString());
+    private BELHeaderWrapper belHeaderWrapper = new BELHeaderWrapper(
+            "Document #" + UUID.randomUUID().toString(),
+            "Converted from BioPAX on " + (new Date()).toString(),
+            "1.0"
+    );
 
     public BELHeaderWrapper getBelHeaderWrapper() {
         return belHeaderWrapper;
@@ -31,6 +40,13 @@ public class BioPAX2BELConverter {
 
     public BELDocument convert(Model bpModel) {
         BELDocument belDocument = getBelUtils().createNewBELDocument(belHeaderWrapper);
+
+        // TODO: Create groups for pathways (make sure you handle multiple layers of pathways)
+        // Create a common group
+        List<BELStatementGroup> statementGroups = belDocument.getBelStatementGroups();
+        BELStatementGroup statementGroup = new BELStatementGroup("BioPAX Reactions");
+        statementGroups.add(statementGroup);
+
         for (BiochemicalReaction reaction : bpModel.getObjects(BiochemicalReaction.class)) {
             // TODO: iterate over all reactions and conver them to BelStatement(s)
         }
